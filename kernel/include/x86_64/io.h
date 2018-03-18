12218f23ebe98459314c3801a9f7e4f56461447b
DropDemBits <ring0hicc@gmail.com>
2018-03-17 23:38:53 -0400

    Enable use of no optimization

--- kernel/include/x86_64/io.h
+++ kernel/include/x86_64/io.h
@@ -3,45 +3,52 @@
 #ifndef __IO_H__
 #define __IO_H__ 1
 
-inline void outb(uint16_t port, uint8_t value)
+#ifdef __NO_OPTIMIZE__
+#	define FPREFIX static
+#else
+#	define FPREFIX inline
+#endif
+
+FPREFIX void outb(uint16_t port, uint8_t value)
 {
 	asm volatile ( "outb %0, %1" : : "a"(value), "Nd"(port) );
 }
 
-inline void outw(uint16_t port, uint16_t value)
+FPREFIX void outw(uint16_t port, uint16_t value)
 {
 	asm volatile ( "outw %0, %1" : : "a"(value), "Nd"(port) );
 }
 
-inline void outl(uint16_t port, uint32_t value)
+FPREFIX void outl(uint16_t port, uint32_t value)
 {
 	asm volatile ( "outl %0, %1" : : "a"(value), "Nd"(port) );
 }
 
-inline uint8_t inb(uint16_t port)
+FPREFIX uint8_t inb(uint16_t port)
 {
 	uint8_t ret;
     asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
     return ret;
 }
 
-inline uint16_t inw(uint16_t port)
+FPREFIX uint16_t inw(uint16_t port)
 {
 	uint16_t ret;
     asm volatile ( "inw %1, %0" : "=a"(ret) : "Nd"(port) );
     return ret;
 }
 
-inline uint32_t inl(uint16_t port)
+FPREFIX uint32_t inl(uint16_t port)
 {
 	uint32_t ret;
     asm volatile ( "inl %1, %0" : "=a"(ret) : "Nd"(port) );
     return ret;
 }
 
-inline void io_wait()
+FPREFIX void io_wait()
 {
 	asm volatile ( "outb %%al, $0x80" : : "a"(0) );
 }
 
+#undef FPREFIX
 #endif
